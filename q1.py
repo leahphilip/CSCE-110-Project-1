61c499e310f46dc6cec91818682d000d16d4268a
leahphilip <96025391+leahphilip@users.noreply.github.com>
2024-01-29 16:43:21 -0600

    Create q1.py

---
+++ q1.py
@@ -0,0 +1,11 @@
+#File: q1.py
+#Author: Leah Philip
+#Date: 01/24/2023
+#Section: 506
+#E-mail: leahephilip@tamu.edu
+#Description: This program takes input from the user and computing their age in four years.
+name = input('Enter first name:\n')
+lastname = input('Enter last name:\n')
+age = int(input('Enter age:\n'))
+new_age= age+4
+print("Howdy", name, lastname,"! You will be",new_age, "in 4 years.")
